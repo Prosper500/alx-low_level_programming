c228de407fc9dda22269b12f255a2b0641b2d702
Prosper500 <prospercallistus@gmail.com>
2023-03-17 13:15:45 -0700

    good

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - printing the single digit of base 10 starting from zero
+ *
+ * Return: (success)
+ */
+
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
