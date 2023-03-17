76b5f1a19e5488fe08d522beb62c03fd6bc65058
Prosper500 <prospercallistus@gmail.com>
2023-03-17 12:05:26 -0700

    good

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - alphabet in lower to upper(case)
+ *
+ * Return: (success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);
+
+	for (low = 'A'; low <= 'z'; low++)
+	putchar(low);
+	putchar('\n');
+
+	return (0);
+}
+
