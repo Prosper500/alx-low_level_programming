86c8b480b0c038d294c47d81e447ee2523649741
Prosper500 <prospercallistus@gmail.com>
2023-03-19 11:21:26 -0700

    good

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A C program that print strings
+ *
+ * Return: (success)
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful - Dora Korpar, 2015-10-19 \n");
+	return (1);
+}
