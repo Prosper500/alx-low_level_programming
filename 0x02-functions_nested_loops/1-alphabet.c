fb6be1f70a9fd6b8e4b83d03b03ecfdf96b2ac53
Prosper500 <prospercallistus@gmail.com>
2023-03-21 14:57:20 -0700

    good

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
- * Return: Always 0.
+ * Return: void
  */
 
 void print_alphabet(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-	{
-	_putchar(low);
-	}
-	_putchar('\n');
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
-
