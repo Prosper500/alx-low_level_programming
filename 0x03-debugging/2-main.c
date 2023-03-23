85060369c7d04ddc65e74899cae229b97bdb5ff6
Prosper500 <prospercallistus@gmail.com>
2023-03-23 05:47:46 -0700

    good

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the largest of 3 integers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, largest;
+
+	a = 972;
+	b = -98;
+	c = 0;
+	largest = (
+	if (a > b && b > c)
+	{
+	argest = a;
+	}
+	else if (b > a && a > c)
+	{
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
+	return (largest);
+	);
+
+	printf("%d is the largest number\n", largest);
+	return (0);
+}
