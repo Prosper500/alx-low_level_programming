0c6e6c0f23c05c260c4e56fe7fd27d81b7e1d426
Prosper500 <prospercallistus@gmail.com>
2023-03-23 02:12:32 -0700

    good

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - testing positve or negative
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
