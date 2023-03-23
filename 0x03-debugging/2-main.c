0a160121fb46e33ecbc210c90187de70129a0d84
Prosper500 <prospercallistus@gmail.com>
2023-03-23 07:11:10 -0700

    good

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -8,27 +8,16 @@
 
 int main(void)
 {
-	int a, b, c, largest;
+	int a, b, c;
+	int largest;
 
 	a = 972;
 	b = -98;
 	c = 0;
-	largest = (
-	if (a > b && b > c)
-	{
-	argest = a;
-	}
-	else if (b > a && a > c)
-	{
-	largest = b;
-	}
-	else
-	{
-	largest = c;
-	}
-	return (largest);
-	);
+
+	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
+
 	return (0);
 }
