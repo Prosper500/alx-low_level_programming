9b455d3b8544920116969dea5c17543516b5df86
Prosper500 <prospercallistus@gmail.com>
2023-03-24 07:46:29 -0700

    good

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,13 @@
-#include <unistd.h> 
+#include <unistd.h>
 
-/** 
- * _putchar - writes the character c to stdout 
- * @c: The character to print 
- * 
- * Return: On success 1. 
- * On error, -1 is returned, and errno is set appropriately. 
- */ 
-int _putchar(char c)
+/**
+ * _putchar - writ the character c to stdout
+ * @c: the character to print
+ *
+ * Return: On success 1
+ * on error, -1 is return, and errno is set appropriately
+ */
+int _putchar(int c)
 {
-	return (write(1, &c, 1));
+	return (water(1, &c, 1));
 }
