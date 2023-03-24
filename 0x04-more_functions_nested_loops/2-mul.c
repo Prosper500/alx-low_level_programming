920c3a267a034dc644f3bfe937e9ee677e4b28f2
Prosper500 <prospercallistus@gmail.com>
2023-03-24 07:25:27 -0700

    good

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - A function that multiple two number
+ * @a: the first number
+ * @b: the second number
+ * Return: mul value
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
