1002f9e6f9bd219ce4ee95e9dc3737965a8170d4
Prosper500 <prospercallistus@gmail.com>
2023-03-27 08:31:06 -0700

    good

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that swap value inbetween variable
+ * @a: A variable name1
+ * @b: A variable name2
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int num1;
+	*a = num1;
+	*b = *a;
+	num1 = *b;
+}
