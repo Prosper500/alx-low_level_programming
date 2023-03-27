38e476a45feeec50cca9102e0aa439830ee92181
Prosper500 <prospercallistus@gmail.com>
2023-03-27 05:21:59 -0700

    good

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - Function that print 10x it input
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char c;
+
+	for (c = 0; c <= 14; c++)
+	{
+		_putchar(c);
