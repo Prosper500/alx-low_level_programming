bf288914447a3af4b70753d3b78be3da32f1c937
Prosper500 <prospercallistus@gmail.com>
2023-03-27 05:19:54 -0700

    good

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A funcrion that reset the value of n
+ * @*n: the check value
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int *n;
+
+	*n = 98;
+}
