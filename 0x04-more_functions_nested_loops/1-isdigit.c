99bd90458df491a3f767dcbf349cf406ecf68637
Prosper500 <prospercallistus@gmail.com>
2023-04-02 14:09:58 -0700

    good

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -8,8 +8,38 @@
 
 int _isdigit(int c)
 {
-	for (c = 0; c <= 9; c++)
+	switch (c)
+	{
+	case 0:
 	return (1);
-	else
+	break;
+	case 1:
+	return (1);
+	break;
+	case 2:
+	return (1);
+	break;
+	case 3:
+	return (1);
+	break;
+	case 4:
+	return (1);
+	break;
+	case 5:
+	return (1);
+	break;
+	case 6:
+	return (1);
+	break;
+	case 7:
+	return (1);
+	break;
+	case 8:
+	return (1);
+	break;
+	case 9:
+	return (1);
+	break;
+	}
 	return (0);
 }
