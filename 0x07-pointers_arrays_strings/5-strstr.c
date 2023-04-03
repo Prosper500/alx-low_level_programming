a73b23f4524d6b35e08adace480d649348c05078
Prosper500 <prospercallistus@gmail.com>
2023-04-03 15:59:30 -0700

    good

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,4 +8,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	char 
+
+	for (; *haystack != '\0'; *haystack++)
+	{
+	char *one = haystack;
+	char *two = needle;
+
+	while (*one == *two && *two != '\0')
+	{
+	*one++;
+	*two++;
+	}
+	if (*two == '\0')
+		return (haystack);
+	}
+
+	return (NULL);
+}
