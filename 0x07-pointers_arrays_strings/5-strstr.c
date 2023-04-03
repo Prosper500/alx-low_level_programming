226f1dec19b6d09027a1b68ba7a3520f7b6e4f2b
Prosper500 <prospercallistus@gmail.com>
2023-04-03 08:28:59 -0700

    good

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strstr- A funcrion that locate a substring
+ * @haystack: inputs
+ * @needle: input
+ * Return: character
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char 
