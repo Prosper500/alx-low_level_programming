a5e16b2fe21b4760f07a354ad5d951127323080c
Prosper500 <prospercallistus@gmail.com>
2023-04-03 07:10:41 -0700

    good

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - A function that copy memory area
+ * @dest: paste destinTion
+ * @src: source memory
+ * @n: byte volume
+ * Return: value digit
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = n, r = 0;
+
+	for (; r < i, r++)
+	{
+		dest[r] = src[r];
+		n--;
+	}
+	return (dest);
+}
