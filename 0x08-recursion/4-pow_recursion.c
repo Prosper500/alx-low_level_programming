bf5662b92ced0881be5f37059741e8f042e8778b
Prosper500 <prospercallistus@gmail.com>
2023-04-05 10:49:03 -0700

    good

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -16,7 +16,5 @@ int _pow_recursion(int x, int y)
 	else if (y == 0)
 		return (1);
 	else if (x < 0 || x > 0)
-		return (_pow_recursion(x, y));
-	else
-		return (-1);
+		return (x * _pow_recursion(x, y - 1));
 }
