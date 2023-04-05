c5f1c6e09b1463e817700ea6f0505301c6774e04
Prosper500 <prospercallistus@gmail.com>
2023-04-05 09:22:05 -0700

    good

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,16 +9,13 @@
  */
 void _puts_recursion(char *s)
 {
-	
 
 	if (*s == '\0')
-	{
-	_putchar('\0');
-	}
-	else
+	_putchar('\n');
+	else if (*s != '\0')
 	{
 	_putchar(*s);
-	*s++;
+	s++;
 	}
 	_putchar('\n');
 }
