e8f44da9d9acc0f4c0fd2ec9444b7299e12006d8
Prosper500 <prospercallistus@gmail.com>
2023-04-05 06:07:44 -0700

    good

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - A funcrion that return power value.
+ * @x: input 1
+ * @y: input 2
+ * Return: integer value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (pow(x, y));
+}
