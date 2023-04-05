7bfc850615b69dc345edead0bed5fd459045f6ed
Prosper500 <prospercallistus@gmail.com>
2023-04-05 16:31:06 -0700

    good

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+int _sqrt(int n, int i);
+/**
+ * _sqrt_recursion - A funtion that print square root of an integer
+ * @n: input integer
+ *
+ * Return: integer value
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ * _sqrt - A function that solve square-root natura number
+ * @n: input
+ * @i: posible result
+ * Return: integer
+ */
+int _sqrt(int n, int i)
+{
+	int sq = i * i;
+
+	if (sq == n)
+		return (i);
+	else if (sq > 0)
+		return (-1);
+	else
+		return (_sqrt(n, i + 1));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_prime_number - A functio that check prime number
+ * @n: the input integer
+ *
+ * Return: interger
+ */
+
+int is_prime_number(int n)
+{
+	n
+}
