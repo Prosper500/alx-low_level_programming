29b4c98ea18b7e98856b1b9474c38877a5fe43fe
Prosper500 <prospercallistus@gmail.com>
2023-04-07 16:38:27 -0700

    good

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,13 +12,16 @@ int main(int argc, char *argv[])
 {
 	int c, mul = 1;
 
-	for (c = 1; c < argc; c++)
-		mul *= atoi(argv[c]);
-	if (mul == 1)
+	if (argc == 3)
 	{
-		printf("Error\n");
-		return (1);
-	}
+	for (c = 1; c < argc; c++)
+	mul *= atoi(argv[c]);
 	printf("%d\n", mul);
+	}
+	else if (argc < 3 || argc > 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
 	return (0);
 }
