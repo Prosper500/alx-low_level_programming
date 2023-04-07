c3ef7929a33a8194a22c6ecccdae9ec5df4f82b4
Prosper500 <prospercallistus@gmail.com>
2023-04-07 11:11:50 -0700

    good

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - A program that print number of argument
+ * @argc: number of argument
+ * @argv: Array of argument entered
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+
+	printf("%d\n", argc -1);
+	return (0);
+}
