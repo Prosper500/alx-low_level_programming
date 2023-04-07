086ccc53d4eb744764a09e75ab29256d24b29739
Prosper500 <prospercallistus@gmail.com>
2023-04-07 07:39:49 -0700

    good

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A programme thta print it name
+ * @argv: Array input
+ * @argc: size of array input
+ *
+ * Return: (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int c = 0;
+
+	if (c < argc)
+	printf("%s\n", argv[c]);
+	
+	return (0);
+}
