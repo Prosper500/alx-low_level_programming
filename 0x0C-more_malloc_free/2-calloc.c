e57a117b827b62e7aafa82c0b42c9aa178b33efb
Prosper500 <prospercallistus@gmail.com>
2023-04-14 08:59:46 -0700

    good

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,6 +3,23 @@
 #include <stdlib.h>
 
 /**
+ * _memset - function with a constant byte
+ * @z: memory size to be filled
+ * @a: character
+ * @n: copy times
+ * Return: character
+ */
+char _memset(char *z, char a, unsingned int n)
+{
+	unsigned int b;
+
+	for (b = 0; b < n; b++)
+	{
+		z[b] = a;
+	}
+	return (z);
+}
+/**
  * _calloc - A function that allocate memory to an array using malloc
  * @nmemb: array element
  * @size: memory size to be allocated
