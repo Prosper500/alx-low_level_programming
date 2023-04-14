510e3561bce05cb743b670294f571365bfc57579
Prosper500 <prospercallistus@gmail.com>
2023-04-14 07:23:19 -0700

    good

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *pt1, *pt2;
 	unsigned int a;
 
-	if ( old_size == new_size)
+	if (old_size == new_size)
 		return (ptr);
 
 	if (new_size == 0 && ptr)
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,7 +11,7 @@
 int *array_range(int min, int max)
 {
 	int *z, a, b;
-	
+
 	if (min > max)
 		return (NULL);
 
