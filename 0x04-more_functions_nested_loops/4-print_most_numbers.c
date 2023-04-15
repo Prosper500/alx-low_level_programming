09463376b3cd719bf3b5329c444d6a3de1ef5f5e
Prosper500 <prospercallistus@gmail.com>
2023-04-15 11:36:01 -0700

    good

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,15 +7,13 @@
  */
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == 2)
-			continue;
-		if (i == 4)
-			continue;
-		_putchar(i);
+	if (i == '2' || i == '4')
+		continue;
+	_putchar(i);
 	}
 	_putchar('\n');
 }
