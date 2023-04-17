9619144564fbf7eb642c5d7dfe875aed695c8352
Prosper500 <prospercallistus@gmail.com>
2023-04-17 11:58:08 -0700

    good

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,13 @@
 
 void print_dog(struct dog *d)
 {
+	if (d != NULL)
+	{
 	if ((*d).name == NULL)
 		d->name = "(nil)";
 	if ((*d).owner == NULL)
 		d->owner = "(nil)";
+	}
 
 	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age, d->owner);
 }
