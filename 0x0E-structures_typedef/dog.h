e4362ba60d5b82aebf5a97e2f455ab15b11f0e03
Prosper500 <prospercallistus@gmail.com>
2023-04-17 08:56:07 -0700

    good

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A dog information
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's own name
+ * Description: 
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/**
+ * typedef - struct user
+ *
+ * Return: 0
+ */
+#endif
