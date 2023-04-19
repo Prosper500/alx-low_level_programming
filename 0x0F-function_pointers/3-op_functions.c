d96f2fc5302207317fb6557b3160e7dcfe67f5b5
Prosper500 <prospercallistus@gmail.com>
2023-04-19 16:08:34 -0700

    good

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+/**
+ * op_add - addition function
+ * @a: input1
+ * @b: input2
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
+
+/**
+ * op_sub - subtraction functiom
+ * @a: input 1
+ * @b: input 2
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	int c;
+
+	c = a - b;
+	return (c);
+}
+
+/**
+ * op_mul - multiplicatio  function
+ * @a: input 1
+ * @b: input 2
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
+/**
+ * op_div - divition function
+ * @a: input 1
+ * @b: input 2
+ * Eeturn: integer
+ */
+int op_div(int a, int b)
+{
+	int c;
+
+	c = a / b;
+	return (c);
+}
+
+/**
+ * op_mod - modula function
+ * @a: input 1
+ * @b: input 2
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	int c;
+
+	c = a % b;
+	return (c);
+}
