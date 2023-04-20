d52fcf34cac8f2300e8e00a964d3ec04fc31c88b
Prosper500 <prospercallistus@gmail.com>
2023-04-20 08:02:01 -0700

    good

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -14,11 +14,12 @@ int (*get_op_func(char *s))(int, int);
 /**
  * struct op - typedef function
  * @op: operator input
+ * @f: function
  */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 
 #endif
