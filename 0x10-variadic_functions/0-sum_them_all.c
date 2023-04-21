3e4d7206f17988ee5ecdc80bb1561dcd385e3d2c
Prosper500 <prospercallistus@gmail.com>
2023-04-21 08:10:26 -0700

    good

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - A function that return the sum of entered perimeter
+ * @n: input
+ *
+ * Return: integer sum value
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list v;
+	unsigned int input1, sum = 0;
+
+	va_start(v, n);
+
+	for(input = 0; input < n; input++)
+		sum = sum + va_arg(v int);
+	va_end(v);
+	return (0);
+}
